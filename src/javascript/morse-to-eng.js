327dfd62d0fafe7c4255992c1e48eac2d764c802
SakkyOP <saksham298@gmail.com>
2022-09-03 22:33:08 +0530

    finished with translating feature

--- src/javascript/morse-to-eng.js
+++ src/javascript/morse-to-eng.js
@@ -50,11 +50,69 @@ function decodeMorse(morseCode) {
       .trim() ;
 }
 
+function encodeMorse(english_text) {
+  var letter = { 
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+  }
+
+   return english_text.split(" ").map( x => [...x].map( y => letter[y] ).join(" ")).join("   ");
+
+}
+
 transButton = document.getElementById("translate-button");
 textBoxes = document.querySelectorAll(".text_bubble");
 
+isMorse = true;
+
 transButton.addEventListener("click", (e)=>{
-  textBoxes.forEach(a => {
-    a.textContent = decodeMorse(a.textContent)
-  })
+  if(isMorse) {
+    isMorse = false;
+    textBoxes.forEach(a => {
+      a.textContent = decodeMorse(a.textContent);
+    });
+    morseIn.value = decodeMorse(morseIn.value);
+  } else {
+    isMorse = true;
+    textBoxes.forEach(a => {
+      a.textContent = encodeMorse(a.textContent);
+    });
+    morseIn.value = encodeMorse(morseIn.value);
+  }
+
+  
 })
